29c62593c5eb38e3eae631b3614549da7bd34ddd
CamiloArias47 <camillo47@gmail.com>
2019-02-10 12:45:42 -0500

    Actualización pagina productos

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -4,19 +4,35 @@
   @endsection
   @section('contenido')
   <div class="wrapper">
-    <div class="page-header section-dark" style="background-image: url('assets/img/underconstruction.png')">
-          <div class="filter"></div>
+    <div class="page-header section-dark" style="background-image: url('assets/img/tienda.jpg')">
+        <div class="filter"></div>
       <div class="content-center">
         <div class="container">
-          <div class="text-right">
-            <h3 class="presentation-title">Tienda en Construcción</h3>
+          <div class="title-brand">
+            <h1 class="presentation-title">Comercio Ambiental</h1>
           </div>
 
-          <h2 class="presentation-subtitle text-right">Pronto podras acceder a una gran variedad de productos amigables con el medio ambiente</h2>
+          <h2 class="presentation-subtitle text-center">
+            Ecoproductos y Ecoservicios.<br/>
+            Enfócate en tu negocio, no pierdas tiempo, nosotros vendemos por ti.
+          </h2>
         </div>
       </div>
-
-      <h6 class="category category-absolute">Ambientes Sostenibles think green </h6>
+      <h6 class="category category-absolute">Ambientes Sostenibles think <span class="text-green">green</span> </h6>
     </div>
   </div>
+
+  <div class="section">
+    <div class="container">
+      <div class="tim-title">
+          <h1>Paisajismo</h1>
+      </div>
+      <div class="row justify-content-end">
+        <div class="col-md-6 font-size-1-2em">
+          <h3>Diseño</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+
   @endsection
